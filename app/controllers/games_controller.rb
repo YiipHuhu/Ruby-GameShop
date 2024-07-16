0740971597da845ab0342db81c4d938fba344a32
YiipHuhu <alangabrielprzyvara@gmail.com>
2024-07-15 23:04:07 -0300

    Implementando kaminari

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,16 @@
 class GamesController < ApplicationController
   def index
-    games = Game.page(params[:page]).per(3)
-    render json: GameSerializer.new(games).serializable_hash.to_json
+    games = Game.page(params[:page])
+    render json: {
+      games: GameSerializer.new(games).serializable_hash,
+      meta: {
+        current_page: games.current_page,
+        next_page: games.next_page,
+        prev_page: games.prev_page,
+        total_pages: games.total_pages,
+        total_count: games.total_count
+      }
+    }
   end
 
   def show
